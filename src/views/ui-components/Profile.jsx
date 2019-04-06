d19ef95465aecc9b3a1e3b947d1f4074b6ab32e2
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-04-06 00:26:17 -0500

    Agregando primera fase del perfil

--- src/views/ui-components/Profile.jsx
+++ src/views/ui-components/Profile.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import {
   Container,
   Card,
+  CardImg,
+  CardSubtitle,
+  Row,
+  Col,
+  Button,
   CardBody,
   CardTitle
 } from 'reactstrap';
+
+// Imagen
+import img7 from '../../assets/images/background/user-bg.jpg';
+import img8 from '../../assets/images/users/8.jpg';
 
 export default class Search extends Component {
   render() {
@@ -21,7 +30,29 @@ export default class Search extends Component {
           </CardBody>
           <CardBody className="border-top">
             <Container>
-              Hello World
+              <Card>
+                <CardImg top width="100%" src={img7} alt="Card image profile" />
+              </Card>
+              <Card>
+                <CardBody>
+                  <CardTitle>Imagen de perfil</CardTitle>
+                  <Row className="mt-3">
+                    <Col xs="6">
+                      <div className="bg-light p-2 border">
+                      <img
+                        src={img8}
+                      />
+                      </div>
+                    </Col>
+                    <Col xs="6">
+                      <div className="bg-light p-2 border">
+                        <Button className="mb-3 btn" outline color="primary">Actualizar imagen de perfil</Button>
+                        <CardSubtitle>El formato debe ser JPEG, PNG.</CardSubtitle>
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
             </Container>
           </CardBody>
         </Card>
