09b9c39ffecb065a7c7d59e96b612e2963f71f4e
teamzz111 <felipeflogxd@gmail.com>
2019-04-07 01:32:37 -0500

    Actualizando componente para listado y búsqueda de usuarios

--- src/views/ui-components/search.jsx
+++ src/views/ui-components/search.jsx
@@ -1,31 +1,92 @@
 import React, { Component } from 'react'
 import {
-  Container,
   Card,
   CardBody,
-  CardTitle
-} from 'reactstrap';
+  CardTitle,
+  FormGroup,
+  Input,
+  Table,
+  Container,
+  Form,
+  Label
+} from "reactstrap";
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jsonResponse: [
+
+        {
+          "paciente": "José Luis",
+          "lugar": "Bogotá",
+          "ocupación": "Desempleado",
+          "cedula": 1012
+        }
+
+      ]
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
+
+
     return (
       <div>
         <Card>
           <CardBody>
-            <CardTitle
-              className="mb-0"
-            >
-              <i className="mdi mdi-pencil-circle mr-2" ></i>
+            <CardTitle className="mb-0">
+              <i className="mdi mdi-pencil-circle mr-2" />
               Buscar Historias
             </CardTitle>
           </CardBody>
           <CardBody className="border-top">
             <Container>
-              Hello World
+              <div className="d-flex align-items-center">
+                <Form className="col-12">
+                  <FormGroup>
+                    <Label for="exampleEmail">
+                      Ingresa el nombre o la cédula
+                    </Label>
+                    <Input
+                      type="text"
+                      name="search"
+                      id="search"
+                      placeholder="Ingresa un dato"
+                    />
+                  </FormGroup>
+                </Form>
+              </div>
+              <Table className="no-wrap v-middle" responsive>
+                <thead>
+                  <tr className="border-0">
+                    <th className="border-0">Paciente</th>
+                    <th className="border-0">Lugar</th>
+                    <th className="border-0">Ocupación</th>
+                    <th className="border-0">Cédula</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.jsonResponse.map(dynamicData => (
+                    <tr className="trow">
+                      <td> {dynamicData.paciente} </td>
+                      <td> {dynamicData.lugar} </td>
+                      <td> {dynamicData.ocupación} </td>
+                      <td> {dynamicData.cedula} </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
             </Container>
           </CardBody>
         </Card>
       </div>
-    )
+    );
   }
 }
