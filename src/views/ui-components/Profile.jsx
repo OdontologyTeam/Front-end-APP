ba9e0a3c79e7522691877f3179fbaeb5d311cf81
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-04-06 21:23:50 -0500

    Bug en el input, no permite editar el campo

--- src/views/ui-components/Profile.jsx
+++ src/views/ui-components/Profile.jsx
@@ -8,7 +8,14 @@ import {
   Col,
   Button,
   CardBody,
-  CardTitle
+  CardTitle,
+  CardHeader,
+  CardText,
+  CardFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input
 } from 'reactstrap';
 
 // Imagen
@@ -16,6 +23,17 @@ import img7 from '../../assets/images/background/user-bg.jpg';
 import img8 from '../../assets/images/users/8.jpg';
 
 export default class Search extends Component {
+  state = {
+    email: 'Maria'
+  }
+
+  handleChangeInput = event => {
+    this.setState({
+      email: event.target.value
+    })
+
+  }
+
   render() {
     return (
       <div>
@@ -37,21 +55,42 @@ export default class Search extends Component {
                 <CardBody>
                   <CardTitle>Imagen de perfil</CardTitle>
                   <Row className="mt-3">
-                    <Col xs="6">
-                      <div className="bg-light p-2 border">
-                      <img
-                        src={img8}
-                      />
-                      </div>
-                    </Col>
-                    <Col xs="6">
-                      <div className="bg-light p-2 border">
-                        <Button className="mb-3 btn" outline color="primary">Actualizar imagen de perfil</Button>
-                        <CardSubtitle>El formato debe ser JPEG, PNG.</CardSubtitle>
-                      </div>
+                    <Col>
+                      <Row className="bg-light p-2 border d-flex">
+                        <Col 
+                          sm={{ size: 'auto', offset: 1 }}
+                          style={{marginLeft: 0}}
+                        >
+                          <img
+                            sm={{ size: 'auto', offset: 1 }}
+                            src={img8}
+                            alt="Foto de perfil"
+                            width={96}
+                            height={96}
+                          />
+                        </Col>
+                        <Col className="mt-2" sm={{ size: 'auto', offset: 1 }}> 
+                          <Button className="mb-3 btn" outline color="primary">Actualizar imagen de perfil</Button>
+                          <CardSubtitle>El formato debe ser JPEG, PNG.</CardSubtitle>
+                        </Col>
+                      </Row>
                     </Col>
                   </Row>
                 </CardBody>
+              </Card>
+              <Card>
+                <CardHeader tag="h5">Cambiar los datos de tu cuenta</CardHeader>
+                <CardBody>
+                  <Form>
+                  <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input type="email" name="email" placeholder="with a placeholder" value={this.state.email} />
+                  </FormGroup>
+                  </Form>
+                </CardBody>
+                <CardFooter className="text-muted">
+                  <Button>Guardar</Button>
+                </CardFooter>
               </Card>
             </Container>
           </CardBody>
